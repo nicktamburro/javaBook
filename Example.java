3b35b9e1f6f2df9167ae3446e9760be4e0679acb
nicktamburro <nicholastamburro@gmail.com>
2018-04-09 22:19:04 -0700

    added a few comments

--- Example.java
+++ Example.java
@@ -1,4 +1,12 @@
+//because you always have to start with a class, which 
+//should be named the same as the file...
 class Example {
+	/*public = access
+	- static allows main to be called before an "object
+	of the class" is created
+	- void tells us that main doesn't return a value
+	- Main is called when the application begins
+	*/
 	public static void main(String args[]){
 	System.out.println("hi hello hi.");
 }
