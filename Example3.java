dabd97a8173d0a6984c7b45778990903bdb40e7b
nicktamburro <nicholastamburro@gmail.com>
2018-04-10 20:27:12 -0700

    made my own gallons to liters solution

---
+++ Example3.java
@@ -0,0 +1,20 @@
+class Example3{
+	public static void main(String args[]){
+
+		int var;
+		double x;
+
+		var = 10;
+		x = 10.0;
+
+		System.out.println("Original var: " + var);
+		System.out.println("Original x: " + x);
+		System.out.println();
+
+		var = var / 4;
+		x = x / 4;
+
+		System.out.println("new var: " + var);
+		System.out.println("new x: " + x);
+	}
+}
---
+++ GallonsToLiters.java
@@ -0,0 +1,23 @@
+class GallonsToLiters{
+	public static void main(String args[]){
+		double gallons;
+		double liters;
+
+		double gallons2;
+		double liters2;
+
+		gallons = 75;
+		liters = gallons * 3.7854;
+		gallons2= 150;
+		liters2 = gallons2 * 3.7854;
+
+	System.out.print(gallons + " gallons is equal to ");
+	System.out.println(liters + " liters!");
+	System.out.println();
+	System.out.print("Also, " + gallons2 + " gallons equals ");
+	System.out.println(liters2 + " liters!!");
+
+
+
+	}
+}
