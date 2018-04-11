63679bc3ff666d3f31f55ead719eb41ed451f407
nicktamburro <nicholastamburro@gmail.com>
2018-04-10 20:37:20 -0700

    java if statements, *slightly* different than I'm used to

---
+++ ifDemo.java
@@ -0,0 +1,31 @@
+class ifDemo{
+	public static void main(String args[]){
+	int a, b, c;
+
+	a = 2;
+	b = 3;
+
+	if(a < b) System.out.println("a is less than b");
+	if(a == b) System.out.println("nothing to see here");
+
+	System.out.println();
+
+	c = a - b;
+
+	System.out.println("c contains " + c);
+	if (c >= 0) System.out.println("c is non-negative");
+	if (c < 0) System.out.println("c is negative");
+
+System.out.println();
+
+	c = b - a;
+
+	System.out.println("c now contains " + c);
+	if (c >= 0) System.out.println("c is non-negative");
+	if (c < 0) System.out.println("c is negative");
+
+
+
+
+	}
+}
