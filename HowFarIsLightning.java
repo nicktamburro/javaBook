23f720a516dff719976a81891294d634e810f966
nicktamburro <nicholastamburro@gmail.com>
2018-04-11 10:56:49 -0700

    more exercises

---
+++ HowFarIsLightning.java
@@ -0,0 +1,11 @@
+class HowFarIsLightning{
+	public static void main(String args[]){
+		double time;
+		double distance;
+
+		time = 7.2;
+		distance = time * 1100;
+
+		System.out.println("If you saw the lighning 7.2 secons ago, the lightning is " + distance + " feet away!");
+	}
+}
---
+++ HowFarIsObject.java
@@ -0,0 +1,17 @@
+class HowFarIsObject{
+	public static void main(String args[]){
+
+//timing the echo can tell us the distance to something;
+
+		double echoTime;
+		double distance;
+
+		echoTime = 0.7;
+		distance = echoTime / 2 * 1100;
+
+		System.out.println("If you heard the echo in 0.7 seconds, the object is " + distance + " feet away!");
+
+
+
+	}
+}
