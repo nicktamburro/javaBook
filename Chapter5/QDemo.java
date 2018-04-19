9c2dbcbb007d97ea1ad6bac955089039afa10a9b
nicktamburro <nicholastamburro@gmail.com>
2018-04-18 21:40:55 -0700

    commented out the queue exercise

--- Chapter5/QDemo.java
+++ Chapter5/QDemo.java
@@ -1,37 +1,41 @@
+//shows the class Queue
 class Queue{
-	char q[];
-	int putloc, getloc;
+	char q[]; //this is the array that acts as queue
+	int putloc, getloc; //the array index that Put and Get will target
 
+//constructor for Queue
 	Queue(int size){
-		q = new char[size];
-		putloc = getloc = 0;
+		q = new char[size];// NOT SURE WHY THIS?
+		putloc = getloc = 0; // sets both to zero at first
 	}
+	//PUT method
 	void put(char ch){
-		if(putloc==q.length){
+		if(putloc==q.length){//if place to put character is the last spot in the array, the array is full
 			System.out.println(" -- Queue is full.");
-			return;
+			return; //so it alerts and takes no action
 		}
-	   q[putloc++] = ch;
+	   q[putloc++] = ch; //if there's room, it puts the character there, but 1[putlocc++] I've never seen
+	   //an increment like that before... 
 }
-	   char get() {
-	   	if(getloc == putloc){
+	   char get() {//GET METHOC
+	   	if(getloc == putloc){   //if the place to Get from is the final spot, there's nothing there to get
 	   		System.out.println(" -- Queue is empty");
-	   		return (char) 0;
+	   		return (char) 0;  //so nothing happens
 	   	}
-	   	return q[getloc++];
+	   	return q[getloc++];  //this again... 
 	   }
 	}
 
 	class QDemo{
 		public static void main(String args[]){
-			Queue bigQ = new Queue(100);
+			Queue bigQ = new Queue(100);   //constructing new Q objects
 			Queue smallQ = new Queue(4);
 			char ch;
 			int i;
 
 			System.out.println("Using bigQ to store alphabet");
-			for(i=0; i<26; i++){
-				bigQ.put((char) ('A' + i));
+			for(i=0; i<26; i++){  
+				bigQ.put((char) ('A' + i));  //a loop, a casee, puts in the alphabet.... 
 			}
 
 			System.out.print("Contents of BigQ: ");
