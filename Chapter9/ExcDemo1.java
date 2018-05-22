34ba2ca024dc42965da2b82ebc33753280408344
nicktamburro <nicholastamburro@gmail.com>
2018-05-21 22:50:36 -0700

    try catch

--- Chapter9/ExcDemo1.java
+++ Chapter9/ExcDemo1.java
@@ -2,13 +2,18 @@ class ExcDemo1{
 	public static void main(String args[]){
 		int nums[] = new int[4];
 
+//so all code that we want to monitor, needs to be enclosed in the "try"
 		try{
 			System.out.println("before exception.");
 
 			//now we make an out-of-bounds exception
 			nums[7] = 10;
+			//it doesn't print this line, because as soon as the exception happens
+			//it "throws" and we're kicked out of this part of the code
+			//and into the "catch" part... 
 			System.out.println("you can't see me");
 		}
+	//so we have to know what exceptions we might get, I guess?
 		catch(ArrayIndexOutOfBoundsException exc){
 			System.out.println("index out-of-bounds");
 		}
