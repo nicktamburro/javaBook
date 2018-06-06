0e84b714a31da002e8c3a152574983d29a7f4fca
nicktamburro <nicholastamburro@gmail.com>
2018-06-06 13:14:41 -0700

    throwable methods

---
+++ Chapter9/UseThrowableMethods.java
@@ -0,0 +1,27 @@
+class ExcTest{
+	static void genException(){
+		int nums[] = new int[4];
+
+		System.out.println("Before exception is generated.");
+
+		//generate in index out-of-bounds exception
+		nums[7] = 10;
+		System.out.println("This won't show.");
+	}
+}
+
+class UseThrowableMethods{
+	public static void main(String args[]){
+		try{
+			ExcTest.genException();
+		}
+		catch (ArrayIndexOutOfBoundsException exc){
+			//catch the exception
+			System.out.println("Standard message is: ");
+			System.out.println(exc);
+			System.out.println("\nStack trace: ");
+			exc.printStackTrace();
+		}
+		System.out.println("After catch statement.");
+	}
+}
