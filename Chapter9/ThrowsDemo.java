27eec1ee3576ba287842ca8e7bf2c50c8c3b71eb
nicktamburro <nicholastamburro@gmail.com>
2018-06-08 19:59:18 -0700

    using throws

---
+++ Chapter9/ThrowsDemo.java
@@ -0,0 +1,22 @@
+class ThrowsDemo{
+	public static char prompt(String str)
+	throws java.io.IOException{
+
+		System.out.print(str + ": ");
+		return (char) System.in.read();
+	}
+
+	public static void main(String args[]){
+		char ch;
+
+		try{
+			ch = prompt("Enter a letter");
+		}
+		catch(java.io.IOException exc){
+			System.out.println("I/0 exception occurred.");
+			ch = 'X';
+		}
+
+			System.out.println("You pressed " + ch);
+	}
+}
