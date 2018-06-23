78c70a7adf4046b53ddd473d32fecb30d27a69b0
nicktamburro <nicholastamburro@gmail.com>
2018-06-22 22:07:04 -0700

    commented out queue

--- Chapter9/FixedQueue.java
+++ Chapter9/FixedQueue.java
@@ -1,21 +1,29 @@
 class FixedQueue implements ICharQ{
+	//I think I might have to rework the file structure here... ICharQ is in the Chapter8
+	//folder
 	private char 1[];
+	//this line looks wrong... =?
 	private int putloc, getloc;
+	//just initializing array indecies
 
 //contruct an empty queue given a size
 	public FixedQueue(int size){
 		 q = new char[size];
 		 putloc = getloc = 0;
+		 //since it starts off empty, they're both at index zero
 	}
 
 //put a character into the queue
-	public void put(char ch)
+	public void put(char ch){
+		//here we make our own new exception
 		throws QueueFullException{
+			//and HERE we define when it happens!
 			if (putloc==q.length)
 				throw new QueueFullException(q.length);
 
 			q[putloc++] = ch;
 		}
+	}
 
 
 }
