93946bedb4d45990e3c7139fa40b08d4777150ee
nicktamburro <nicholastamburro@gmail.com>
2018-09-06 22:51:44 -0700

    copy/pasted something crazy, trying to unbreak it

--- Chapter4/3DPointDemo.java
+++ Chapter4/3DPointDemo.java
@@ -1,9 +1,10 @@
 public class Point{
+	public static void Main(String args[]){
 	double x;
 	double y;
 	double z;
 
-	public Point(double x, double y, double z){
+	 Point(double x, double y, double z){
 		this.x = x;
 		this.y = y;
 		this.z = z;
@@ -17,9 +18,16 @@ public class Point{
 		return Math.sqrt(dx * dx + dy * dy + dz * dz);
 	}
 
-Point p1 = new Point(23.5, 56.4, 87.2);
-Point p2 = new Point(1.1, 100.1, 10.1);
 
+//so these are not contained in anything, they need to be in their own thing!
+//Point p1 = new Point(23.5, 56.4, 87.2);
+//Point p2 = new Point(1.1, 100.1, 10.1);
+}
 }
 
+double Points{
+	Point p1 = new Point(23.5, 56.4, 87.2);
+    Point p2 = new Point(1.1, 100.1, 10.1);
 
+    System.out.println(p1.distance + " " + p2.distance);
+}
