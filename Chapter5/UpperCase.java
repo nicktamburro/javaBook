1860610de9df94a05abc85098a9362d4d09a80e5
nicktamburro <nicholastamburro@gmail.com>
2018-09-18 11:07:11 -0700

    playing with bits and bytes

---
+++ Chapter5/UpperCase.java
@@ -0,0 +1,15 @@
+class UpperCase{
+    public static void main(String[] args){
+        char ch;
+
+        for(int i=0; i<10;i++){
+            ch = (char) ('a' + i);
+            System.out.print(ch);
+
+            //this statement "turns off" the 6th bit
+            ch = (char) ((int) ch & 65503);
+
+            System.out.print(ch + " ");
+        }
+    }
+}
